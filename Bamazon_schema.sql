1a46fe23029bd0e44c99e616f9c66d3037e2e878
geo_ep <geoffrey.xwin@gmail.com>
2019-07-13 01:50:12 -0700

    updated bamazonCustomer

--- Bamazon_schema.sql
+++ Bamazon_schema.sql
@@ -5,10 +5,12 @@ USE bamazon;
 CREATE TABLE products (
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR (50) NOT NULL,
-deparment_name VARCHAR (50) NOT NULL,
+department_name VARCHAR (50) NOT NULL,
 price DECIMAL (6,2) DEFAULT 0,
 stock_quantity INT DEFAULT 0,
 PRIMARY KEY (item_id)
 );
+
+alter table products add index deparment_name(department_name);
 
 SELECT * FROM products;
